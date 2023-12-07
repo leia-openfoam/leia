12cadad688cd9c029b703cc691fe6058ba36451c
Julian Reitzel <j.reitzel@outlook.de>
2023-12-07 16:45:35 +0100

    UPDATE: src/finiteVolume/finiteVolume/gradSchemes/noBvGrad: added member function modified_leastSquaresGrad_calcGrad, class modified_leastSquaresVectors With the additional member function and class the assambled least squares linear system is manipulated in a way that boundary faces are not in the system.

--- src/finiteVolume/finiteVolume/gradSchemes/noBvGrad/noBvGrad.C
+++ src/finiteVolume/finiteVolume/gradSchemes/noBvGrad/noBvGrad.C
@@ -27,12 +27,191 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "noBvGrad.H"
-
-// #include "extrapolatedCalculatedFvPatchField.H"
+#include "MeshObject.H"
+#include "surfaceFields.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
+namespace Foam
+{
+class modified_leastSquaresVectors
+:
+    public MeshObject<fvMesh, MoveableMeshObject, modified_leastSquaresVectors>
+{
 
+    // Private Data
+
+        //- Owner least-squares gradient vectors
+        surfaceVectorField pVectors_;
+
+        //- Neighbour least-squares gradient vectors
+        surfaceVectorField nVectors_;
+
+
+    // Private Member Functions
+
+        //- Construct Least-squares gradient vectors
+        void calcLeastSquaresVectors()
+        {
+            DebugInFunction << "Calculating least square gradient vectors" << nl;
+
+            const fvMesh& mesh = mesh_;
+
+            // Set local references to mesh data
+            const labelUList& owner = mesh_.owner();
+            const labelUList& neighbour = mesh_.neighbour();
+
+            const volVectorField& C = mesh.C();
+
+            // Set up temporary storage for the dd tensor (before inversion)
+            symmTensorField dd(mesh_.nCells(), Zero);
+
+            forAll(owner, facei)
+            {
+                const label own = owner[facei];
+                const label nei = neighbour[facei];
+
+                const vector d(C[nei] - C[own]);
+                const symmTensor wdd(sqr(d)/magSqr(d));
+                dd[own] += wdd;
+                dd[nei] += wdd;
+            }
+
+
+            // surfaceVectorField::Boundary& blsP =
+            //     pVectors_.boundaryFieldRef();
+
+            // forAll(blsP, patchi)
+            // {
+            //     const fvsPatchVectorField& patchLsP = blsP[patchi];
+
+            //     const fvPatch& p = patchLsP.patch();
+            //     const labelUList& faceCells = p.patch().faceCells();
+
+            //     // Build the d-vectors
+            //     const vectorField pd(p.delta());
+
+            //     forAll(pd, patchFacei)
+            //     {
+            //         const vector& d = pd[patchFacei];
+
+            //         dd[faceCells[patchFacei]] += sqr(d)/magSqr(d);
+            //     }
+            // }
+
+
+            // Invert the dd tensor
+            const symmTensorField invDd(inv(dd));
+
+
+            // Revisit all faces and calculate the pVectors_ and nVectors_ vectors
+            forAll(owner, facei)
+            {
+                const label own = owner[facei];
+                const label nei = neighbour[facei];
+
+                const vector d(C[nei] - C[own]);
+
+                pVectors_[facei] = (invDd[own] & d)/magSqr(d);
+                nVectors_[facei] = -(invDd[nei] & d)/magSqr(d);
+            }
+
+            // forAll(blsP, patchi)
+            // {
+            //     fvsPatchVectorField& patchLsP = blsP[patchi];
+
+            //     const fvPatch& p = patchLsP.patch();
+            //     const labelUList& faceCells = p.faceCells();
+
+            //     // Build the d-vectors
+            //     const vectorField pd(p.delta());
+
+            //     forAll(pd, patchFacei)
+            //     {
+            //         const vector& d = pd[patchFacei];
+
+            //         patchLsP[patchFacei] = (invDd[faceCells[patchFacei]] & d)/magSqr(d);
+            //     }
+            // }
+
+            DebugInfo << "Finished calculating least square gradient vectors" << endl;
+        }
+
+
+public:
+
+    // Declare name of the class and its debug switch
+    TypeName("modified_leastSquaresVectors");
+
+
+    // Constructors
+
+        //- Construct given an fvMesh
+        explicit modified_leastSquaresVectors(const fvMesh& mesh)
+        :
+            MeshObject<fvMesh, Foam::MoveableMeshObject, modified_leastSquaresVectors>(mesh),
+            pVectors_
+            (
+                IOobject
+                (
+                    "LeastSquaresP",
+                    mesh_.pointsInstance(),
+                    mesh_,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE,
+                    false
+                ),
+                mesh_,
+                dimensionedVector(dimless/dimLength, Zero)
+            ),
+            nVectors_
+            (
+                IOobject
+                (
+                    "LeastSquaresN",
+                    mesh_.pointsInstance(),
+                    mesh_,
+                    IOobject::NO_READ,
+                    IOobject::NO_WRITE,
+                    false
+                ),
+                mesh_,
+                dimensionedVector(dimless/dimLength, Zero)
+            )
+        {
+            calcLeastSquaresVectors();
+        }
+
+
+    //- Destructor
+    virtual ~modified_leastSquaresVectors(){}
+
+    // Member functions
+
+        //- Return const reference to owner least square vectors
+        const surfaceVectorField& pVectors() const
+        {
+            return pVectors_;
+        }
+
+        //- Return const reference to neighbour least square vectors
+        const surfaceVectorField& nVectors() const
+        {
+            return nVectors_;
+        }
+
+        //- Delete the least square vectors when the mesh moves
+        virtual bool movePoints()
+        {
+            calcLeastSquaresVectors();
+            return true;
+        }
+};
+// }
+// namespace Foam
+// {
+    defineTypeNameAndDebug(modified_leastSquaresVectors, 0);
+}
 
 
 template<class Type>
@@ -54,11 +233,137 @@ Foam::fv::noBvGrad<Type>::calcGrad
     typedef typename outerProduct<vector, Type>::type GradType;
     typedef GeometricField<GradType, fvPatchField, volMesh> GradFieldType;
 
-    tmp<GradFieldType> tgGrad = tinnerGradScheme_().calcGrad(vsf, name);
+    // Delegate gradient calculation 
+    tmp<GradFieldType> tgrad = tinnerGradScheme_().calcGrad(vsf, name);
+    
+    // GradFieldType& grad = tgrad.ref();
+    // auto& gradbf = grad.boundaryFieldRef();
 
-    return tgGrad;
+    // // Fix gradients in boundary cells
+
+    // // fvc::reconstruct(fvc::snGrad(vsf)*mesh.magSf())
+
+    // forAll(vsf.boundaryField(), patchi)
+    // {
+    //     const fvPatchField&     pfield = vsf.boundaryField()[patchi];
+    //     fvPatchField::Internal& ifield = vsf.boundaryField()[patchi].internalField();
+
+    //     if (!vsf.boundaryField()[patchi].coupled())
+    //     {
+    //         forAll(pfield, facei)
+    //         {
+    //             // grad(own(face)) = fvc::snGrad(oppositeFace(face)
+    //             ifield.[facei] = 
+    //         }
+
+
+    //     }
+    // }
+
+
+    return tgrad;
 }
 
-
+template<class Type>
+Foam::tmp
+<
+    Foam::GeometricField
+    <
+        typename Foam::outerProduct<Foam::vector, Type>::type,
+        Foam::fvPatchField,
+        Foam::volMesh
+    >
+>
+Foam::fv::noBvGrad<Type>::modified_leastSquaresGrad_calcGrad
+(
+    const GeometricField<Type, fvPatchField, volMesh>& vsf,
+    const word& name
+) const
+{
+    typedef typename outerProduct<vector, Type>::type GradType;
+    typedef GeometricField<GradType, fvPatchField, volMesh> GradFieldType;
+ 
+    const fvMesh& mesh = vsf.mesh();
+ 
+    tmp<GradFieldType> tlsGrad
+    (
+        new GradFieldType
+        (
+            IOobject
+            (
+                name,
+                vsf.instance(),
+                mesh,
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            mesh,
+            dimensioned<GradType>(vsf.dimensions()/dimLength, Zero),
+            extrapolatedCalculatedFvPatchField<GradType>::typeName
+        )
+    );
+    GradFieldType& lsGrad = tlsGrad.ref();
+ 
+    // Get reference to least square vectors
+    const modified_leastSquaresVectors& lsv = modified_leastSquaresVectors::New(mesh);
+ 
+    const surfaceVectorField& ownLs = lsv.pVectors();
+    const surfaceVectorField& neiLs = lsv.nVectors();
+ 
+    const labelUList& own = mesh.owner();
+    const labelUList& nei = mesh.neighbour();
+ 
+    forAll(own, facei)
+    {
+        const label ownFacei = own[facei];
+        const label neiFacei = nei[facei];
+ 
+        const Type deltaVsf(vsf[neiFacei] - vsf[ownFacei]);
+ 
+        lsGrad[ownFacei] += ownLs[facei]*deltaVsf;
+        lsGrad[neiFacei] -= neiLs[facei]*deltaVsf;
+    }
+ 
+    // Boundary faces
+    forAll(vsf.boundaryField(), patchi)
+    {
+        const fvsPatchVectorField& patchOwnLs = ownLs.boundaryField()[patchi];
+ 
+        const labelUList& faceCells =
+            vsf.boundaryField()[patchi].patch().faceCells();
+ 
+        if (vsf.boundaryField()[patchi].coupled())
+        {
+            const Field<Type> neiVsf
+            (
+                vsf.boundaryField()[patchi].patchNeighbourField()
+            );
+ 
+            forAll(neiVsf, patchFacei)
+            {
+                lsGrad[faceCells[patchFacei]] +=
+                    patchOwnLs[patchFacei]
+                   *(neiVsf[patchFacei] - vsf[faceCells[patchFacei]]);
+            }
+        }
+        else
+        {
+            // const fvPatchField<Type>& patchVsf = vsf.boundaryField()[patchi];
+ 
+            // forAll(patchVsf, patchFacei)
+            // {
+            //     lsGrad[faceCells[patchFacei]] +=
+            //          patchOwnLs[patchFacei]
+            //         *(patchVsf[patchFacei] - vsf[faceCells[patchFacei]]);
+            // }
+        }
+    }
+ 
+ 
+    lsGrad.correctBoundaryConditions();
+    gaussGrad<Type>::correctBoundaryConditions(vsf, lsGrad);
+ 
+    return tlsGrad;
+}
 
 // ************************************************************************* //
