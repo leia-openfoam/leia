8c238b9fd3e5fa3fb379c9a7c573bb542a2d3b0c
Julian Reitzel <j.reitzel@outlook.de>
2023-12-19 15:06:55 +0100

    RM: cases/2Dcontactline-vortex-smallMesh

--- cases/2Dcontactline-vortex_smallMesh/AllplotAndCollect.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-readonly SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
-readonly DIR_PATH="$(dirname $SCRIPT_PATH)"
-
-mkdir -p plots
-study_cases.py . | xargs -I{} bash -c 'cd {} && python ../2Dcontactline-vortex/plot.py && cp plot.png ../plots/{}.png'
-cp study_*.json plots/
