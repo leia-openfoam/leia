a66c19e29802a1d430f4b4c58e6ace56a05e68eb
Tomislav Maric <tomislav.maric@gmx.com>
2025-01-22 19:07:22 +0100

    [defect-correction] taylorCouette field errors finished

---
+++ cases/defect-correction/taylorCouette/taylorCouetteFunctionObject/taylorCouetteFlow.C
@@ -0,0 +1,125 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2025 Tomislav Maric, TU Darmstadt 
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Class
+    Foam::taylorCouetteFlow
+
+Description
+
+    A concrete class implementing the Taylor-Couette flow, i.e. the flow 
+    between two rotating cylinders.
+
+SourceFiles
+    taylorCouetteFlow.C
+
+\*---------------------------------------------------------------------------*/
+
+#include "taylorCouetteFlow.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+taylorCouetteFlow::taylorCouetteFlow
+(
+    const scalar Ri, 
+    const scalar Ro, 
+    const scalar& omegai, 
+    const scalar& omegao, 
+    const vector& axisPoint
+)
+: 
+    Ri_(Ri),
+    sqrRi_(sqr(Ri_)),
+    Ro_(Ro),
+    sqrRo_(sqr(Ro_)),
+    omegai_(omegai),
+    omegao_(omegao),
+    axisPoint_(axisPoint)
+{}
+
+taylorCouetteFlow::taylorCouetteFlow(const dictionary& dict)
+: 
+    Ri_(dict.getOrDefault<scalar>("Ri", 1.0)),
+    sqrRi_(sqr(Ri_)),
+    Ro_(dict.getOrDefault<scalar>("Ro", 2.0)),
+    sqrRo_(sqr(Ro_)),
+    omegai_(dict.getOrDefault<scalar>("omegai", 1)),
+    omegao_(dict.getOrDefault<scalar>("omegao", 0)),
+    axisPoint_(dict.getOrDefault<vector>("axisPoint", vector(0,0,0)))
+{}
+
+vector taylorCouetteFlow::velocityCylindrical(const scalar r) const
+{
+    scalar u_theta = r*(omegao_*sqrRo_ - omegai_*sqrRi_) / (sqrRo_ - sqrRi_) - 
+	((omegao_ - omegai_) * sqrRi_*sqrRo_)/(r * (sqrRo_ - sqrRi_));
+
+    return vector(0, u_theta, 0); 
+}
+
+vector taylorCouetteFlow::velocityCartesian(const vector& x) const
+{
+    // Compute radial distance of x from the axis point.
+    scalar r = mag(x - axisPoint_);
+    if (r == 0) // then undefined transformation at the origin.
+    {
+       return vector(0,0,0); 
+    }
+
+    // Compute cylindrical velocity 
+    const vector uCylindrical = velocityCylindrical(r);
+
+    // Compute trigonometric terms
+    scalar cosTheta = x[0] / r;
+    scalar sinTheta = x[1] / r;
+
+    // Convert to Cartesian components
+    scalar u_x = -uCylindrical[1]* sinTheta;
+    scalar u_y = uCylindrical[1]* cosTheta;
+
+    return vector(u_x, u_y, 0);
+}
+
+scalar taylorCouetteFlow::pressure(const scalar r) const
+{
+    // TODO(TM)
+    return 0; 
+}
+
+scalar taylorCouetteFlow::pressureCartesian(const vector& pos) const
+{
+    // TODO(TM)
+    return 0; 
+}
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
---
+++ cases/defect-correction/taylorCouette/taylorCouetteFunctionObject/taylorCouetteFunctionObject.C
@@ -0,0 +1,192 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2025 AUTHOR,AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "taylorCouetteFunctionObject.H"
+#include "Time.H"
+#include "fvMesh.H"
+#include "addToRunTimeSelectionTable.H"
+#include "surfaceFields.H"
+#include "OFstream.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(taylorCouetteFunctionObject, 0);
+    addToRunTimeSelectionTable(functionObject, taylorCouetteFunctionObject, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::taylorCouetteFunctionObject::taylorCouetteFunctionObject
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    fvMeshFunctionObject(name, runTime, dict),
+    tcFlow_(dict), 
+    pTaylorCouette_ 
+    (
+        IOobject(
+            "pTaylorCouette",
+            runTime.timeName(),
+            mesh_,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh_,
+        dimensionedScalar("dimensionedPressure", dimPressure, 0.0)
+    ),
+    UTaylorCouette_
+    (
+        IOobject
+        (
+            "UTaylorCouette",
+            runTime.timeName(), 
+            mesh_,
+            IOobject::NO_READ,
+            IOobject::AUTO_WRITE
+        ),
+        mesh_,
+        dimensionedVector("dimensionedVelocity", dimVelocity, vector::zero)
+    ),
+    Uerr_("Uerr", UTaylorCouette_)
+{
+    // Compute the Taylor-Couette pressure and velocity fields 
+    
+    // Access the mesh cell centers
+    const vectorField& C = mesh_.C();
+
+    // Iterate over all cell-centered values 
+    forAll(C, cellI)
+    {
+        // Get the position of the cell center
+        const vector& xc = C[cellI];
+        vector xcCylindrical (xc[0], xc[1], 0);
+
+        // Compute and store the velocity and pressure for the cell
+        UTaylorCouette_[cellI] = tcFlow_.velocityCartesian(xcCylindrical);
+        pTaylorCouette_[cellI] = tcFlow_.pressureCartesian(xcCylindrical);
+    }
+
+    // Iterate over all boundary patches for face-centered values
+    forAll(UTaylorCouette_.boundaryField(), patchI)
+    {
+        // Access the current boundary patch
+        fvPatchVectorField& velocityPatch = UTaylorCouette_.boundaryFieldRef()[patchI];
+        fvPatchScalarField& pressurePatch = pTaylorCouette_.boundaryFieldRef()[patchI];
+
+        // Access the face centers of the boundary patch
+        const vectorField& faceCenters = velocityPatch.patch().Cf();
+
+        // Iterate over all faces on the current patch
+        forAll(faceCenters, faceI)
+        {
+            const vector& xf = faceCenters[faceI];
+
+            vector xfCylindrical (xf[0], xf[1], 0);
+
+            // DEBUGGING
+            const scalar r = mag(xfCylindrical); 
+            Info << "face center = " << xfCylindrical << endl;
+            Info << "r = " << r << endl; 
+            Info << "velocity cylindrical = " << tcFlow_.velocityCylindrical(r) << endl;
+            Info << "velocity cartesian = " << tcFlow_.velocityCartesian(xfCylindrical) << endl;
+            Info << "velocity cartesian mag = " << mag(tcFlow_.velocityCartesian(xfCylindrical)) << endl;
+            // DEBUGGING
+
+            // Compute and set the velocity and pressure for the boundary face
+            velocityPatch[faceI] = tcFlow_.velocityCartesian(xfCylindrical);
+            pressurePatch[faceI] = tcFlow_.pressureCartesian(xfCylindrical);
+        }
+    }
+
+    // Compute the velocity error. 
+    const volVectorField& U = mesh_.lookupObject<volVectorField>("U");
+    Uerr_ == U - UTaylorCouette_; 
+    
+    // Write fields
+    UTaylorCouette_.write(); 
+    pTaylorCouette_.write();
+    Uerr_.write();
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::functionObjects::taylorCouetteFunctionObject::read(const dictionary& dict)
+{
+    return true;
+}
+
+
+bool Foam::functionObjects::taylorCouetteFunctionObject::execute()
+{
+    // Compute the velocity error. 
+    const volVectorField& U = mesh_.lookupObject<volVectorField>("U");
+    Uerr_ == U - UTaylorCouette_; 
+    return true;
+}
+
+
+bool Foam::functionObjects::taylorCouetteFunctionObject::end()
+{
+    volScalarField UerrMag ("UerrMag", mag(Uerr_));
+
+    if (Pstream::myProcNo() == 0)
+    {
+        scalar UerrLinf = gMax(UerrMag);
+	scalar UerrL1 = gSum((UerrMag * mesh_.V())());
+	scalar UerrL2 = sqrt(gSum((UerrMag * UerrMag * mesh_.V())()));
+	scalar h = gMax(Foam::pow(mesh_.deltaCoeffs(), -1)());
+
+	OFstream of("TaylorCouette.csv");
+	of << "h,UerrLinf,UerrL1,UerrL2\n"
+            << h << "," << UerrLinf << "," 
+	    << UerrL1 << "," << UerrL2 << "\n";
+    }
+    
+    return true;
+}
+
+
+bool Foam::functionObjects::taylorCouetteFunctionObject::write()
+{
+    pTaylorCouette_.write();
+    UTaylorCouette_.write();
+    Uerr_.write();
+    return true;
+}
+
+
+// ************************************************************************* //
